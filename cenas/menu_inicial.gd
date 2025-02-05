592376650f20e048403267720c05a642f752c530
Jonas Correia <jonas149.correia@gmail.com>
2025-02-05 15:51:02 -0300

    cenario completo
    dois cenarios da neve

--- cenas/menu_inicial.gd
+++ cenas/menu_inicial.gd
@@ -2,29 +2,17 @@ extends Node2D
 
 var somEnabled = true
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_inicia_button_pressed() -> void:
-	var text = $LineEdit.text
-	if (text):
-		print("Bem vindo " + text)
-		get_tree().change_scene_to_file("res://cenas/world.tscn")
-	else:
-		print("Por favor, digite seu nome para iniciarmos")
+	var text = $LineEdit.text.strip_edges()
+	
+	if text.is_empty():
 		$Label.text = "Por favor, digite seu nome para iniciarmos"
+		$Label.show()  # Garante que o Label fique visível
+	else:
+		print("Bem-vindo, " + text)
+		get_tree().change_scene_to_file("res://cenas/world.tscn")
 
 
 func _on_som_button_pressed() -> void:
 	somEnabled = !somEnabled
-	if (somEnabled):
-		$BotaoSom.self_modulate.a = 1
-	else:
-		$BotaoSom.self_modulate.a = 0.2
+	$BotaoSom.self_modulate.a = 1 if somEnabled else 0.2
--- cenas/world.gd
+++ cenas/world.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+
 # Carrega a cena da letra que será usada para criar instâncias
 var Letter = preload("res://cenas/letter.tscn")
 
 # Função chamada quando o nó é inicializado
 func _ready():
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	# Configura a interface do usuário
 	setup_ui()
 	# Inicia a primeira palavra do jogo
