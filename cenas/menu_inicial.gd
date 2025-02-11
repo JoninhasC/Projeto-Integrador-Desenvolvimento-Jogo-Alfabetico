b7f7f27274bfab5fd095e429468a5b326d1af431
bren0Silv4 <brenobsilva68@gmail.com>
2025-02-10 22:13:45 -0300

    implementação da musica no menu inicial e na fase da neve.
    obs: o pause no esc nao está funcionando aqui.
    Co-Authored-By: Bárbara Lopes <105359621+barbaraclopes@users.noreply.github.com>

--- cenas/menu_inicial.gd
+++ cenas/menu_inicial.gd
@@ -1,18 +1,34 @@
 extends Node2D
 
 var somEnabled = true
+@export var som: Node
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
 
 func _on_inicia_button_pressed() -> void:
-	var text = $LineEdit.text.strip_edges()
-	
-	if text.is_empty():
-		$Label.text = "Por favor, digite seu nome para iniciarmos"
-		$Label.show()  # Garante que o Label fique visível
-	else:
-		print("Bem-vindo, " + text)
+	var text = $LineEdit.text
+	if (text):
+		print("Bem vindo " + text)
 		get_tree().change_scene_to_file("res://cenas/world.tscn")
+	else:
+		print("Por favor, digite seu nome para iniciarmos")
+		$Label.text = "Por favor, digite seu nome para iniciarmos"
+
+@onready var menuInicial = $BotaoSom/menu_incial as AudioStreamPlayer
 
 
 func _on_som_button_pressed() -> void:
 	somEnabled = !somEnabled
-	$BotaoSom.self_modulate.a = 1 if somEnabled else 0.2
+	if (somEnabled):
+		som.stream_paused = false
+		$BotaoSom.self_modulate.a = 1
+	else:
+		som.stream_paused = true
+		$BotaoSom.self_modulate.a = 0.2
